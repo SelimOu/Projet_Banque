4050fce80a0b114e8375d0b0b444cff1e507034c
Selim <ounoughi.selim@gmail.com>
2024-09-17 11:25:40 +0200

    optimisatimisations pour le deploiement

--- backend/database/migrations/2024_09_04_091706_add_two_factor_columns_to_users_table.php
+++ backend/database/migrations/2024_09_04_091706_add_two_factor_columns_to_users_table.php
@@ -13,15 +13,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('two_factor_secret')
-                ->after('password')
-                ->nullable();
+            // Vérification pour éviter la duplication de colonnes
+            if (!Schema::hasColumn('users', 'two_factor_secret')) {
+                $table->text('two_factor_secret')
+                    ->after('password')
+                    ->nullable();
+            }
 
-            $table->text('two_factor_recovery_codes')
-                ->after('two_factor_secret')
-                ->nullable();
+            if (!Schema::hasColumn('users', 'two_factor_recovery_codes')) {
+                $table->text('two_factor_recovery_codes')
+                    ->after('two_factor_secret')
+                    ->nullable();
+            }
 
-            if (Fortify::confirmsTwoFactorAuthentication()) {
+            if (Fortify::confirmsTwoFactorAuthentication() && !Schema::hasColumn('users', 'two_factor_confirmed_at')) {
                 $table->timestamp('two_factor_confirmed_at')
                     ->after('two_factor_recovery_codes')
                     ->nullable();
@@ -35,12 +40,14 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(array_merge([
-                'two_factor_secret',
-                'two_factor_recovery_codes',
-            ], Fortify::confirmsTwoFactorAuthentication() ? [
-                'two_factor_confirmed_at',
-            ] : []));
+            // Supprimer les colonnes si elles existent
+            $columnsToDrop = ['two_factor_secret', 'two_factor_recovery_codes'];
+
+            if (Fortify::confirmsTwoFactorAuthentication()) {
+                $columnsToDrop[] = 'two_factor_confirmed_at';
+            }
+
+            $table->dropColumn($columnsToDrop);
         });
     }
 };
